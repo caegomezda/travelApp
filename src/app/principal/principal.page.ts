3146b04f0357c981b3e9f73cb924e02e322c2f1a
SantiagoBarreroC <santiagobarrerocalderon@gmail.com>
2022-04-05 15:46:20 -0500

    estilo del login-integracion de mapa

--- src/app/principal/principal.page.ts
+++ src/app/principal/principal.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var google;
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.page.html',
   styleUrls: ['./principal.page.scss'],
 })
 export class PrincipalPage implements OnInit {
-
+  map = null;
   constructor() { }
 
+  loadMap() {
+    // create a new map by passing HTMLElement
+    const mapEle: HTMLElement = document.getElementById('map');
+    // create LatLng object
+    const myLatLng = {lat: 5.058859029717547, lng: -75.48927077310586};
+    // create map
+    this.map = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 17
+    });
+  
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      // this.renderMarkers();
+      mapEle.classList.add('show-map');
+    });
+  }
+
   ngOnInit() {
+    this.loadMap();
   }
 
 }
