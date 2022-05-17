5329a87599f431d675a21d097ba9086a61b30074
unknown <santiagobarrerocalderon@gmail.com>
2022-05-17 15:27:19 -0500

    alert de driver

--- src/app/driver/driver.page.ts
+++ src/app/driver/driver.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ComponentsIonicService } from '../zservices/components-ionic.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-driver',
@@ -8,11 +10,41 @@ import { ComponentsIonicService } from '../zservices/components-ionic.service';
 })
 export class DriverPage implements OnInit {
 
-  constructor(public componentsIonic: ComponentsIonicService) { }
+  constructor(public alertController: AlertController,
+    private router: Router) { }
 
   ngOnInit() {
   }
-  presentarAlerta(){
-    this.componentsIonic.presentAlertConfirm();
+  presentarAlerta() {
+    this.presentAlertConfirm();
+  }
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      backdropDismiss: false,
+      header: 'Camilo Gomez',
+      mode: 'ios',
+      message: '<ion-icon name="location"></ion-icon><strong>Calle 60 #45 b-21</strong>',
+      buttons: [
+        {
+          text: 'Rechazar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          id: 'cancel-button',
+
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Aceptar',
+          id: 'confirm-button',
+          handler: () => {
+            this.router.navigateByUrl('/driver-map', { replaceUrl: true });
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 }
